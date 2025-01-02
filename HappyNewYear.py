198a7507f7245a120c3958875abb2ea237e913a6
zageabb <zageabb@users.noreply.github.com>
2025-01-02 11:18:19 +0000

    Update HappyNewYear.py

--- HappyNewYear.py
+++ HappyNewYear.py
@@ -7,7 +7,7 @@ st.title("Dancing Santa 🎅")
 st.markdown(
     """
     <div style="text-align: center;">
-        <img src="https://raw.githubusercontent.com/zageabb/XmasTimer/refs/heads/main/happy-christmas-dancing-santa.gif" alt="Dancing Santa" style="width: 100%; max-width: 600px;">
+        <img src="https://raw.githubusercontent.com/zageabb/HappyNewYear/refs/heads/main/hny2025.gif" alt="Dancing Santa" style="width: 100%; max-width: 600px;">
     </div>
     """,
     unsafe_allow_html=True,
